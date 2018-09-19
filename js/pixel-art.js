01333645ceaa689ba30b8b8fbf082e3c3b38f6a9
Jose Ignacio Di Marco <joseignaciodimarco@MacBook-Pro-2.local>
2018-09-19 19:47:14 -0300

    Se ordeno el codigo para que se pueda leer con facilidad y se implemento la funcion que detecta cuando se hace clic y suelta sobre la grilla

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -20,9 +20,38 @@ var nombreColores = ['White', 'LightYellow',
   'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
 ];
 
+//INICIO DECLARACION DE VARIABLES//
+/*************************************************************/
 // Variable para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
+
+//Variable clickMouse -> true cuando esta apretado sobre la grilla, false en caso contrario
+var clickMouse = false;
+
+//Variable que guarda el elemento con id -> paleta
+var paleta = document.getElementById('paleta');
+
+//Variable que guarda el elemento con id -> grilla-pixeles
+var grilla = document.getElementById('grilla-pixeles');
+/*************************************************************/
+//FIN DECLARACION DE VARIABLES//
+
+//INICIO LISTA DE LISTENERS//
+/*************************************************************/
+//Listener clic del mouse sobre un div de la paleta de colores
+paleta.addEventListener('click', cambiaColorIndicador);
+
+//Listener clic pixel
+grilla.addEventListener('click', pintaPixel);
+
+//Listener que detecta click del mouse en grilla
+grilla.addEventListener('mousedown', presionaClick);
+
+//Listener que detecta cuando se suelta click del mouse en grilla
+grilla.addEventListener('mouseup', sueltaClick);
+/*************************************************************/
+//FIN LISTA DE LISTENERS//
 
 colorPersonalizado.addEventListener('change', 
   (function() {
@@ -31,12 +60,6 @@ colorPersonalizado.addEventListener('change',
     document.getElementById('indicador-de-color').style.backgroundColor = colorActual;
   })
 );
-
-//Variable que guarda el elemento con id -> paleta
-var paleta = document.getElementById('paleta');
-
-//Variable que guarda el elemento con id -> grilla-pixeles
-var grilla = document.getElementById('grilla-pixeles');
 
 //Funcion que genera la paleta de colores
 function generaPaleta() {
@@ -62,14 +85,6 @@ function generaGrillaPixeles(){
 //Llama a la funcion generaGrillaPixeles
 generaGrillaPixeles();
 
-//LISTA DE LISTENERS
-// // // // // // // // //
-//Listener clic del mouse sobre un div de la paleta de colores
-paleta.addEventListener('click', cambiaColorIndicador);
-
-//Listener clic pixel
-grilla.addEventListener('click', pintaPixel);
-
 //Funcion que cambia el color de fondo del indicador de color
 function cambiaColorIndicador(e){
   document.getElementById('indicador-de-color').style.backgroundColor = e.target.style.backgroundColor;
@@ -84,4 +99,14 @@ function pintaPixel(e){
   e.target.style.backgroundColor = document.getElementById('indicador-de-color').style.backgroundColor;
 }
 
+//Funcion que asigna valor true a la variable clickMouse cuando se hace clic en la grilla
+function presionaClick(){
+  console.log('hace click');
+  clickMouse = true;
+}
 
+//Funcion que asigna valor false a la variable clickMouse cuando se suelta clic en la grilla
+function sueltaClick(){
+  console.log('suelta click');
+  clickMouse = false;
+}
